91f289e384720fca45221adda956416e948e56a8
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-08-15 14:34:42 -0600

    SMFL Inclusion

--- GalvanEngine/GalvanEngine/src/GalvanEngine.cpp
+++ GalvanEngine/GalvanEngine/src/GalvanEngine.cpp
@@ -26,3 +26,27 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
 */
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Mi primera ventana SFML");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+  return 0;
+}

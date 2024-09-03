b13d9d6b2b1fc51d730296e980bc2e9d70fc80c9
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-09-03 08:46:17 -0600

    BaseApp Abstraction and Implementation

--- GalvanEngine/GalvanEngine/include/BaseApp.h
+++ GalvanEngine/GalvanEngine/include/BaseApp.h
@@ -26,10 +26,39 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
 */
-#include "BaseApp.h"
+#pragma once
+#include <SFML/Graphics.hpp>
 
-int 
-main() {
-	BaseApp app;
-	return app.run();
-}
+class
+BaseApp {
+public:
+	BaseApp() = default;
+	~BaseApp() = default;
+
+	// Funcion encargada de ejecutar la aplicacion en main
+	int
+	run();
+
+	// Funcion de inicializacion
+	void
+	initialize();
+
+	// Funcion de manejo de datos
+	void
+	handleEvents();
+
+	// Funcion que se actualiza por frame
+	void
+	update();
+
+	// Funcion de renderizado
+	void
+	render();
+
+	void
+	cleanup();
+
+private:
+	sf::RenderWindow* window;
+	sf::CircleShape* shape;
+};
--- GalvanEngine/GalvanEngine/src/BaseApp.cpp
+++ GalvanEngine/GalvanEngine/src/BaseApp.cpp
@@ -29,7 +29,49 @@
 #include "BaseApp.h"
 
 int 
-main() {
-	BaseApp app;
-	return app.run();
+BaseApp::run() {
+	initialize();
+	while (window->isOpen())
+	{
+		handleEvents();
+		update();
+		render();
+	}
+
+	cleanup();
+	return 0;
+}
+
+void
+BaseApp::initialize() {
+	window = new sf::RenderWindow(sf::VideoMode(800, 600), "Galvan Engine");
+	shape = new sf::CircleShape(100.0f);
+	shape->setFillColor(sf::Color::Green);
+}
+
+void 
+BaseApp::handleEvents() {
+	sf::Event event;
+	while (window->pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window->close();
+	}
+}
+
+void 
+BaseApp::update() {
+}
+
+void 
+BaseApp::render() {
+	window->clear();
+	window->draw(*shape);
+	window->display();
+}
+
+void 
+BaseApp::cleanup() {
+	delete window;
+	delete shape;
 }
